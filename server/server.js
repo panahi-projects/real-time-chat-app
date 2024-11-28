933253bebf0c10baf3ec43da7a0e97b4f1c8ad21
SAEED.PANAHI <panahi.projects@gmail.com>
2024-11-28 12:43:06 +0330

    establish the socket.io connection

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const PORT = 3001;
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User is connected with id: ${socket.id}`);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
